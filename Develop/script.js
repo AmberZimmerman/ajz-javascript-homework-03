b83925862dc9d6e0e8cfafdf8b376a04b62b335e
Amber Zimmerman <ajz1221@gmail.com>
2022-01-09 20:54:40 -0700

    adding functions and if statements into writepassword function

--- Develop/script.js
+++ Develop/script.js
@@ -4,59 +4,62 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-// Create generate password
-  var passwordText = document.querySelector("#password");
+  
+function generatePassword() {
+  
+//Prompt with Criteria for the password
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
-  passwordText.value = password;
+let charSet = "";
 
-  //Prompt to choose the length of the password that is at least 8 characters and no more than 124 characters
-function lengthQuestion() {
-  let lengthAnswer = prompt("How many characters long would you like your password to be?");
-  console.log(lengthAnswer);
-}
-lengthQuestion()
+//Prompt to choose the length of the password that is at least 8 characters and no more than 124 characters
+let lengthAnswer = prompt("How many characters long would you like your password to be? Type a number between 8 and 124", "8");
+  console.log(`length of password ${lengthAnswer}`);
 
 //Prompt to choose confirm whether or not to include lowercases
-function lowercaseQuestion() {
-  let lowercaseAnswer = prompt("Would you like your password to include lowercase letters?");
-  console.log(lowercaseAnswer);
+if (confirm("Would you like your password to include lowercase letters?")) {
+  console.log(`Include lowercase letters ${lowercaseAnswer}`);
 }
-lowercaseQuestion()
+
 
 
 //Prompt to choose confirm whether or not to include uppercase
 function uppercaseAnswer() {
-  let uppercaseAnswer = prompt("Would you like your password to include uppercase letters?");
-  console.log(uppercaseAnswer);
+  let uppercaseAnswer = confirm("Would you like your password to include uppercase letters?");
+  console.log(`Include uppercase letters ${uppercaseAnswer}`);
 }
 uppercaseAnswer()
 
 
 //Prompt to choose confirm whether or not to include numbers
 function numberQuestion() {
-  let numberAnswer = prompt("Would you like your password to include numbers?");
-  console.log(numberAnswer);
+  let numberAnswer = confirm("Would you like your password to include numbers?");
+  console.log(`include numbers ${numberAnswer}`);
 }
 numberQuestion()
 
 //Prompt to choose confirm whether or not to include special characters
 function characterQuestion() {
-  let characterAnswer = prompt("Would you like your password to include special characters?");
-  console.log(characterAnswer);
+  let characterAnswer = confirm("Would you like your password to include special characters?");
+  console.log(`Include special characters ${characterAnswer}`);
 }
 characterQuestion()
+  
+  
+
+  
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-//Prompt with Criteria for the password
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
 
 
 
@@ -69,3 +72,11 @@ const symbols = "!@#$%^&*_-+=";
 
 //Generated password displayed in an alert of written to the page
 
+
+
+// charSet = "";
+
+// if upperCase === true {
+//   charSet += const alpha
+//   console.log(charset) abdoicj
+// }
