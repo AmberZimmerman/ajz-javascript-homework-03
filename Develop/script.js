23eab58fb94e319a5a8af26f36e5d9d8101321a7
Amber Zimmerman <ajz1221@gmail.com>
2022-01-09 19:27:21 -0700

    clarifying function names and calling functions for password critera prompts

--- Develop/script.js
+++ Develop/script.js
@@ -4,9 +4,47 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+// Create generate password
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  //Prompt to choose the length of the password that is at least 8 characters and no more than 124 characters
+function lengthQuestion() {
+  let lengthAnswer = prompt("How many characters long would you like your password to be?");
+  console.log(lengthAnswer);
+}
+lengthQuestion()
+
+//Prompt to choose confirm whether or not to include lowercases
+function lowercaseQuestion() {
+  let lowercaseAnswer = prompt("Would you like your password to include lowercase letters?");
+  console.log(lowercaseAnswer);
+}
+lowercaseQuestion()
+
+
+//Prompt to choose confirm whether or not to include uppercase
+function uppercaseAnswer() {
+  let uppercaseAnswer = prompt("Would you like your password to include uppercase letters?");
+  console.log(uppercaseAnswer);
+}
+uppercaseAnswer()
+
+
+//Prompt to choose confirm whether or not to include numbers
+function numberQuestion() {
+  let numberAnswer = prompt("Would you like your password to include numbers?");
+  console.log(numberAnswer);
+}
+numberQuestion()
+
+//Prompt to choose confirm whether or not to include special characters
+function characterQuestion() {
+  let characterAnswer = prompt("Would you like your password to include special characters?");
+  console.log(characterAnswer);
+}
+characterQuestion()
 
 }
 
@@ -15,36 +53,14 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //Prompt with Criteria for the password
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
 
-//Prompt to choose the length of the password that is at least 8 characters and no more than 124 characters
-function test() {
-  let lengthAnswer = prompt("How many characters long would you like your password to be?");
-  console.log(lengthAnswer);
-}
 
-//Prompt to choose confirm whether or not to include lowercases
-function test() {
-  let lowercaseAnswer = prompt("Would you like your password to include lowercase letters?");
-  console.log(lowercaseAnswer);
-}
 
-//Prompt to choose confirm whether or not to include uppercase
-function test() {
-  let uppercaseAnswer = prompt("Would you like your password to include uppercase letters?");
-  console.log(uppercaseAnswer);
-}
-
-//Prompt to choose confirm whether or not to include numbers
-function test() {
-  let alphaAnswer = prompt("Would you like your password to include numbers?");
-
-//Prompt to choose confirm whether or not to include special characters
-function test() {
-  let alphaAnswer = prompt("Would you like your password to include special characters?");
 
 
 // When a user answsers a prompt - each input type should be validated and at least one character type should be selected
