59700f5a3328b8370c15636f19f303b1ea4a2474
Amber Zimmerman <ajz1221@gmail.com>
2022-01-09 22:29:49 -0700

    taking code outside of the given function and creating a new function for generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -4,19 +4,23 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
 function generatePassword() {
-  
+
 //Prompt with Criteria for the password
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
+// When a user answsers a prompt - each input type should be validated and at least one character type should be selected and will be added to charSet
 let charSet = "";
 
 //Prompt to choose the length of the password that is at least 8 characters and no more than 124 characters
-let lengthAnswer = prompt("How many characters long would you like your password to be? Type a number between 8 and 124", "8");
+let lengthAnswer = Number(prompt("How many characters long would you like your password to be? Type a number between 8 and 124", "8"));
   console.log(`length of password ${lengthAnswer}`);
 
 //Prompt to choose confirm whether or not to include lowercases
@@ -39,45 +43,28 @@ if (confirm("Would you like your password to include numbers?")) {
 
 
 //Prompt to choose confirm whether or not to include special characters
- if (confirm("Would you like your password to include special characters?")) {
+if (confirm("Would you like your password to include special characters?")) {
   charSet += symbols;
   console.log(charSet);
 }
 
 let finalPassword = "";
- for (index = 0, index < Math.floor(Math.random() * )
-  
-  
 
-  
-  var passwordText = document.querySelector("#password");
+//When prompts are answered, generate a password that matches the criteria
+for (i = 0; i < lengthAnswer; i++) {
+  finalPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));
+}
 
-  passwordText.value = password;
+//Generated password displayed in an alert or written to the page
 
-  return finalPassword;
+console.log(finalPassword);
+return finalPassword;
 
 }
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// THEN my input should be validated and at least one character type should be selected WHEN all prompts are answered
 
 
-
-
-
-
-// When a user answsers a prompt - each input type should be validated and at least one character type should be selected
-
-//When prompts are answered, generate a password that matches the criteria
-
-//Generated password displayed in an alert of written to the page
-
-
-
-// charSet = "";
-
-// if upperCase === true {
-//   charSet += const alpha
-//   console.log(charset) abdoicj
-// }
